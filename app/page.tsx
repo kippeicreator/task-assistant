712164cef0c405b1fba837f5763b240aae10fbea
石丸桔平 <kippei@MacBook-Kir.local>
2026-06-04 08:09:08 +0900

    add deadline calculation

--- app/page.tsx
+++ app/page.tsx
@@ -17,21 +17,57 @@ export default function Home() {
       return;
     }
 
+    const today = new Date();
+    const dueDate = new Date(deadline);
+
+    const diffTime = dueDate.getTime() - today.getTime();
+    const daysLeft = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if (daysLeft <= 3) {
+  setResult(`課題名　${taskName}
+
+締切まであと ${daysLeft} 日
+
+緊急モード
+
+今日
+・課題内容確認
+・資料収集
+
+明日
+・作業完了
+
+締切前日
+・最終確認`);
+  return;
+}
+
     if (taskName.includes("レポート")) {
       setResult(`課題名　${taskName}
+
+締切まであと ${daysLeft} 日
+
 今日
 ・テーマを決める
+
 明日
 ・参考文献を3本集める
+
 3日目
 ・構成を作る
+
 締切：${deadline}`);
     } else if (taskName.includes("発表")) {
       setResult(`課題名　${taskName}
+
+締切まであと ${daysLeft} 日
+
 今日
 ・テーマ整理
+
 明日
 ・スライド作成
+
 3日目
 ・発表練習`);
     } else {
